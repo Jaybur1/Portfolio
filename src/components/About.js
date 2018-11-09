03661b29f8c4f7bea17179587047f41eb9617c54
Evgeniy Burbyga <jayburbyga@gmail.com>
2018-11-09 15:10:44 -0400

    hello

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,21 @@ render(){
           <h1 className="quote">"Try to be better then yourself tomorrow !"</h1>
           <hr />
           <div className="about-content">
-          <p> Coming soon ... </p>
+          <p> who am I :
+Hi! 
+My name is Evgeniy Burbyga , but you can call me Jay (my wife and mom calling me that). I am 28 years old, was born in the Ukraine , moved to Israel with my family at the age of 6 , and then moved to Canada with my wife in 2017.
+I have a cat named Muffin and I playing drums sice 2004.
+
+ 
+Early life:
+ 
+before I dicided to make web development as my primary proffesion, I was a profesional truck driver, and web development was a side hoby that i really enjoyed. 
+some time along the way this hoby became a passion, and a way of life . 
+with that I realized that learning and self development is the thing that keeps my gears going.
+
+what am i upto now :
+
+as the quote on top of this page says, i am trying to be better then tommorow me , Im constently learning and self developing. </p>
         </div>
         </div>
     )
